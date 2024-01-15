2a5469ab1b9f86dfafad62e3022ae0a1be0f3c0d
Jesse Landis <jesse.landis@ufl.edu>
2024-01-15 16:29:09 -0500

    added a scrper to find arbitrages

--- src/test.py
+++ src/test.py
@@ -1,35 +1,35 @@
-import time
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.common.keys import Keys
+import requests
+import xlsxwriter
+import pandas as pd
+import numpy as np
+import openpyxl
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Border, Side, Font, Alignment, PatternFill, numbers
 
-URL = 'https://sportsbook.fanduel.com'
-driver = uc.Chrome()
-driver.implicitly_wait(5)
-driver.get(URL)
-time.sleep(5.0)
-print(driver.find_element(By.XPATH, "//a[contains(@href,'/live')]").click())
-time.sleep(2.0)
+API_KEY = '5f9cc44e136f665467faac18bddb88d1'
 
-driver.find_element(By.XPATH, "//a[contains(@href,'/live')]//span[text()='" + "Tennis" + "']").click()
+def getNextBets():
+    SPORT = 'upcoming' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
 
-time.sleep(3.0)
+    REGIONS = 'us' # uk | us | eu | au. Multiple can be specified if comma delimited
 
+    MARKETS = 'h2h' # h2h | spreads | totals. Multiple can be specified if comma delimited
 
+    ODDS_FORMAT = 'decimal' # decimal | american
 
+    DATE_FORMAT = 'iso' # iso | unix
 
+    BET_SIZE = 10
 
-
-
-
-
-
-input("Press Enter to close the browser...")
-
-
-
-# Close the browser window
-driver.quit()
-
+    return requests.get(
+        f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
+        params={
+            'api_key': API_KEY,
+            'regions': REGIONS,
+            'markets': MARKETS,
+            'oddsFormat': ODDS_FORMAT,
+            'dateFormat': DATE_FORMAT,
+        }
+    ).json()
+    
+print(getNextBets()[0])
